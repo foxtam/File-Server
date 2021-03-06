56cdce99d8b720394a0ff6ced0b07d546f909a87
Foxtam <foxtam7@ya.ru>
2021-03-06 18:35:10 +0300

    Move client and server

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -1,21 +1,19 @@
-package server.server;
+package client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Main {
     private static final String host = "127.0.0.1";
     private static final int port = 34567;
-    private static final String sendMessage = "All files were sent!";
+    private static final String sendMessage = "Give me everything you have!";
 
     public static void main(String[] args) throws IOException {
-        try (ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName(host))) {
-            System.out.println("Server started!");
-            Socket socket = serverSocket.accept();
+        try (Socket socket = new Socket(InetAddress.getByName(host), port)) {
+            System.out.println("Client started!");
             communicate(socket);
         }
     }
@@ -25,11 +23,11 @@ public class Server {
         DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
         try (socket; dataInputStream; dataOutputStream) {
-            String inputMsg = dataInputStream.readUTF();
-            System.out.println("Received: " + inputMsg);
-
             dataOutputStream.writeUTF(sendMessage);
             System.out.println("Sent: " + sendMessage);
+
+            String inputMsg = dataInputStream.readUTF();
+            System.out.println("Received: " + inputMsg);
         }
     }
 }
