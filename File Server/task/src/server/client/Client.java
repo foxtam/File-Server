5ef3b6d53a0d1c817822d4a1da5ce8aae2bdb602
Foxtam <foxtam7@ya.ru>
2021-03-06 18:20:09 +0300

    Add client-server

---
+++ File Server/task/src/server/client/Client.java
@@ -0,0 +1,33 @@
+package server.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+    private static final String host = "127.0.0.1";
+    private static final int port = 34567;
+    private static final String sendMessage = "Give me everything you have!";
+
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket(InetAddress.getByName(host), port)) {
+            System.out.println("Client started!");
+            communicate(socket);
+        }
+    }
+
+    private static void communicate(Socket socket) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        try (socket; dataInputStream; dataOutputStream) {
+            dataOutputStream.writeUTF(sendMessage);
+            System.out.println("Sent: " + sendMessage);
+
+            String inputMsg = dataInputStream.readUTF();
+            System.out.println("Received: " + inputMsg);
+        }
+    }
+}
---
+++ File Server/task/src/server/server/Server.java
@@ -0,0 +1,35 @@
+package server.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static final String host = "127.0.0.1";
+    private static final int port = 34567;
+    private static final String sendMessage = "All files were sent!";
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName(host))) {
+            System.out.println("Server started!");
+            Socket socket = serverSocket.accept();
+            communicate(socket);
+        }
+    }
+
+    private static void communicate(Socket socket) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        try (socket; dataInputStream; dataOutputStream) {
+            String inputMsg = dataInputStream.readUTF();
+            System.out.println("Received: " + inputMsg);
+
+            dataOutputStream.writeUTF(sendMessage);
+            System.out.println("Sent: " + sendMessage);
+        }
+    }
+}
