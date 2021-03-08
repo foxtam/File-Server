73a7d42ee33c5ab6aa1be4954939fb0abc4b0af2
Foxtam <foxtam7@ya.ru>
2021-03-08 16:12:56 +0300

    Complete server.Main

--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -10,27 +10,17 @@ import java.net.Socket;
 public class Main {
     private static final String host = "127.0.0.1";
     private static final int port = 34567;
-    private static final String sendMessage = "All files were sent!";
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName(host))) {
             System.out.println("Server started!");
-            try (Socket socket = serverSocket.accept()) {
-                communicate(socket);
+
+            try (Socket socket = serverSocket.accept();
+                 var input = new DataInputStream(socket.getInputStream());
+                 var output = new DataOutputStream(socket.getOutputStream())) {
+
+                new ServerCommunicator(input, output).run();
             }
-        }
-    }
-
-    private static void communicate(Socket socket) throws IOException {
-        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-
-        try (dataInputStream; dataOutputStream) {
-            String inputMsg = dataInputStream.readUTF();
-            System.out.println("Received: " + inputMsg);
-
-            dataOutputStream.writeUTF(sendMessage);
-            System.out.println("Sent: " + sendMessage);
         }
     }
 }
