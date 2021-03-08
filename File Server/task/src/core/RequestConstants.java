cb290ea965c494d537f48589261ea236d6f19386
Foxtam <foxtam7@ya.ru>
2021-03-08 14:39:44 +0300

    Add class RequestConstants

---
+++ File Server/task/src/core/RequestConstants.java
@@ -0,0 +1,7 @@
+package core;
+
+public class RequestConstants {
+    public static final String GET = "GET";
+    public static final String PUT = "PUT";
+    public static final String DEL = "DEL";
+}
