9dec7057805a4be41a0ac28bd581f571c506f578
Foxtam <foxtam7@ya.ru>
2021-03-24 10:47:00 +0300

    Delete classes RequestConstants, Response, DelRequest, GetRequest, PutRequest, Request

--- File Server/task/src/core/RequestConstants.java
+++
@@ -1,8 +0,0 @@
-package core;
-
-public class RequestConstants {
-    public static final String GET = "GET";
-    public static final String PUT = "PUT";
-    public static final String DEL = "DEL";
-    public static final String EXIT = "EXIT";
-}
