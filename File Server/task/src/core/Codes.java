2a5c8694c6021ba17cc1534c628533ed4c818ea0
Foxtam <foxtam7@ya.ru>
2021-03-24 10:38:10 +0300

    Add class Codes

---
+++ File Server/task/src/core/Codes.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Codes {
+    public static final int NO_FILE_CODE = 404;
+    public static final int OK_CODE = 200;
+    public static final int FILE_ALREADY_EXISTS_CODE = 403;
+}
