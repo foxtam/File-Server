7e46892f43a2cdca52f877d47bd5d16e12cb9581
Foxtam <foxtam7@ya.ru>
2021-03-24 10:38:39 +0300

    Add class RequestType

---
+++ File Server/task/src/core/RequestType.java
@@ -0,0 +1,8 @@
+package core;
+
+public class RequestType {
+    public static final String GET = "GET";
+    public static final String PUT = "PUT";
+    public static final String DEL = "DEL";
+    public static final String EXIT = "EXIT";
+}
